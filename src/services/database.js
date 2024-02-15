6492fb04f23e45687d24232875371f56b7ec52c7
Rafael Camargo <contato@rafaelcamargo.com>
2024-02-15 10:11:24 -0300

    chore: configure neon adapter for prisma orm

--- src/services/database.js
+++ src/services/database.js
@@ -1,9 +1,12 @@
+const { Pool, neonConfig } = require('@neondatabase/serverless');
+const { PrismaNeon } = require('@prisma/adapter-neon');
 const { PrismaClient } = require('@prisma/client');
+const ws = require('ws');
 const errorService = require('./error');
 
 const _public = {};
 
-_public.dbClient = new PrismaClient();
+_public.dbClient = buildDbClient();
 
 _public.handleTransaction = (request, onSuccess, res) => {
   request()
@@ -13,5 +16,22 @@ _public.handleTransaction = (request, onSuccess, res) => {
       res.status(500).send(err);
     });
 };
+
+function buildDbClient(){
+  return {
+    'production': buildProdDbClient()
+  }[process.env.NODE_ENV] || buildDefaultDbClient();
+}
+
+function buildDefaultDbClient(){
+  return new PrismaClient();
+}
+
+function buildProdDbClient(){
+  neonConfig.webSocketConstructor = ws;
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaNeon(pool);
+  return new PrismaClient({ adapter });
+}
 
 module.exports = _public;
